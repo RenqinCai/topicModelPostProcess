86c7d2dccd551b40aee5eaf49400046e07ffb473
RenqinCai <jetcai1900@gmail.com>
2017-04-06 16:14:00 -0400

    add articleSim4Corpus

--- histogram/articleSim4Corpus.py
+++ histogram/articleSim4Corpus.py
@@ -1,9 +1,17 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import os
 
 class _Corpus:
 	def __init__(self):
+		self.m_articleList = [] ###article Obj
+		self.m_validArticleList = []
+		self.m_invalidArticleList = []
+
+class _Article:
+	def __init__(self, name):
+		self.m_name = name
 		self.m_wordMap = {} ####vocabulary
 		self.m_wordList = []
 		self.m_totalSim = 0
@@ -24,7 +32,7 @@ class _ArticleWord:
 		self.m_name = name
 		self.m_simList = []
 
-def obtainTokenList(fileName, corpusObj):
+def obtainTokenList(fileName, articleObj):
 	f = open(fileName)
 
 	rawLine = f.readline()
@@ -35,9 +43,11 @@ def obtainTokenList(fileName, corpusObj):
 		simUnit = line[i]
 		simUnit = simUnit.split(":")
 
-		corpusObj.m_articleWordList.append(simUnit[0])
+		articleObj.m_articleWordList.append(simUnit[0])
 
-def readFile(fileName, corpusObj):
+	f.close()
+
+def readFile(fileName, articleObj):
 	f = open(fileName)
 
 	for rawLine in f:
@@ -45,8 +55,8 @@ def readFile(fileName, corpusObj):
 		word = line[0]
 
 		wordObj = _Word(word)
-		corpusObj.m_wordMap.setdefault(word, wordObj)
-		corpusObj.m_wordList.append(word)
+		articleObj.m_wordMap.setdefault(word, wordObj)
+		articleObj.m_wordList.append(word)
 		
 		lineLen = len(line)
 		for i in range(1, lineLen):
@@ -57,43 +67,49 @@ def readFile(fileName, corpusObj):
 
 			# wordObj.m_articleTotalSim += (float(simUnit[1])-corpusObj.m_minSim)/(corpusObj.m_maxSim-corpusObj.m_minSim)
 			# wordObj.m_articleTotalSim += float(simUnit[1])
-
-
 	f.close()
 
-def simStatistics(corpusObj):
-	articleSimRatioList = []
-
-	wordNuminArticle = 0
-	firstWordFlag = True
-
+def simStatistics(corpusObj, threshold):
+	
 	totalSim = 0
 	avgSim = 0
 
-	for articleWordStr in corpusObj.m_articleWordList:
-		totalSim = 0
-		for wordStr in corpusObj.m_wordMap.keys():
-			wordObj = corpusObj.m_wordMap[wordStr]
+	for articleObj in corpusObj.m_articleList:
+		articleSimRatioList = []
+		wordNuminArticle = 0
+		firstWordFlag = True
 
-			wordObj.m_articleTotalSim = (wordObj.m_wordSimMap[articleWordStr]-corpusObj.m_minSim)/(corpusObj.m_maxSim-corpusObj.m_minSim)
+		for articleWordStr in articleObj.m_articleWordList:
+			totalSim = 0
+			for wordStr in articleObj.m_wordMap.keys():
+				wordObj = articleObj.m_wordMap[wordStr]
 
-			# wordObj.m_articleTotalSim = wordObj.m_wordSimMap[articleWordStr]
+				wordObj.m_articleTotalSim = (wordObj.m_wordSimMap[articleWordStr]-articleObj.m_minSim)/(articleObj.m_maxSim-articleObj.m_minSim)
 
-			if firstWordFlag:
-				wordNuminArticle = len(wordObj.m_wordSimMap.keys())
-				firstWordFlag = False
-			else:
-				if wordNuminArticle != len(wordObj.m_wordSimMap.keys()):
-					print "wordStr========"
-					print "error wordNuminArticle\t", wordNuminArticle, "\t", len(wordObj.m_wordSimMap.keys())
+				# wordObj.m_articleTotalSim = wordObj.m_wordSimMap[articleWordStr]
 
-			totalSim += wordObj.m_articleTotalSim
+				if firstWordFlag:
+					wordNuminArticle = len(wordObj.m_wordSimMap.keys())
+					firstWordFlag = False
+				else:
+					if wordNuminArticle != len(wordObj.m_wordSimMap.keys()):
+						print "wordStr========"
+						print "error wordNuminArticle\t", wordNuminArticle, "\t", len(wordObj.m_wordSimMap.keys())
 
-		for wordStr in corpusObj.m_wordMap.keys():
-			wordObj = corpusObj.m_wordMap[wordStr]
-			simRatio = wordObj.m_articleTotalSim/totalSim
-			articleSimRatioList.append(simRatio)
+				totalSim += wordObj.m_articleTotalSim
 
+			for wordStr in articleObj.m_wordMap.keys():
+				wordObj = articleObj.m_wordMap[wordStr]
+				simRatio = wordObj.m_articleTotalSim/totalSim
+				articleSimRatioList.append(simRatio)
+
+		sortedArticleSimRatioList = sorted(articleSimRatioList)
+		minArticleSim = sortedArticleSimRatioList[0]
+		maxArticleSim = sortedArticleSimRatioList[-1]
+		if minArticleSim < threshold and threshold < maxArticleSim:
+			corpusObj.m_validArticleList.append(articleObj)
+		else:
+			corpusObj.m_invalidArticleList.append(articleObj)
 
 	# avgSim = totalSim/(len(corpusObj.m_wordMap.keys())*1.0)
 
@@ -101,7 +117,13 @@ def simStatistics(corpusObj):
 	# print "word num\t", len(corpusObj.m_wordMap.keys())
 	# print "avgTotalSim\t", avgSim
 	# print "article sim ratio list\t", articleSimRatioList
-	return articleSimRatioList
+	print "valid article obj =============="
+	for articleObj in corpusObj.m_validArticleList:
+		print articleObj.m_name
+
+	print "invalid article obj =============="
+	for articleObj in corpusObj.m_invalidArticleList:
+		print articleObj.m_name 
 
 def plotStatistics(articleSimRatioList):
 	sortedArticleSimRatioList = sorted(articleSimRatioList)
@@ -119,11 +141,18 @@ def plotStatistics(articleSimRatioList):
 	plt.title("total sim for word before stemming")
 	plt.show()
 
+def readDir(dirName, corpusObj, threshold):
+	for fileName in os.listdir(dirName):
+		if fileName.endswith(".txt"):
+			articleObj = _Article(fileName)
+			corpusObj.m_articleList.append(articleObj)
+			obtainTokenList(os.path.join(dirName, fileName), articleObj)
+			readFile(os.path.join(dirName, fileName), articleObj)
+	simStatistics(corpusObj, threshold)
+
 fileName = "444_article_beforeStemming.txt"
 corpusObj = _Corpus()
 
-obtainTokenList(fileName, corpusObj)
-readFile(fileName, corpusObj)
-
-articleSimRatioList = simStatistics(corpusObj)
-plotStatistics(articleSimRatioList)
+threshold = 1.0/5745
+dirName = "./articleSim"
+readDir(dirName, corpusObj, threshold)
